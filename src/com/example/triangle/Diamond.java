4285932a2489f6d099cac57e2aa8a728200c86f2
katiepetersonn <katie.peterson19@gmail.com>
2017-09-15 22:16:00 +1000

    diamond

--- src/com/example/triangle/Diamond.java
+++ src/com/example/triangle/Diamond.java
@@ -2,14 +2,14 @@ package com.example.triangle;
 
 public class Diamond {
     public static void main(String[] args) {
-        int number = 10;
+        int number = 3;
         IsoscelesTriangle.createTriangle(number);
-        drawBottomDiamond(number);
+        drawBottomDiamond(number + 1 );
     }
 
     public static void drawBottomDiamond(int size){
         for (int x = size - 2; x >= 0; x--) {
-            for (int y = 0; y <= size - x; y++) {
+            for (int y = 3; y <= size - x; y++) {
                 System.out.print(" ");
             }
             for (int z = 0; z <= 2 * x; z++) {
