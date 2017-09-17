75a6f9e0221891f359a2d0e006dbd0526186a704
katiepetersonn <katie.peterson19@gmail.com>
2017-09-17 15:02:16 +1000

    fizzbuzz

--- src/com/example/triangle/FizzBuzz.java
+++ src/com/example/triangle/FizzBuzz.java
@@ -1,4 +1,22 @@
 package com.example.triangle;
 
 public class FizzBuzz {
+    public static void main(String args[]){
+        for (int i = 1; i <= 100; i++){
+            System.out.println(changeNumber(i));
+        }
+    }
+
+    public static String changeNumber(int number){
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "Fizzbuzz";
+        }
+        if (number % 3 == 0){
+            return "Fizz";
+        }
+        if (number % 5 == 0){
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
 }
